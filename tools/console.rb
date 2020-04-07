b4eb8d89971fa584ac911c23e8a2e10e79369453
coryrosser <coryrosser@gmail.com>
2020-04-07 11:10:49 -0400

    CODE CHALLENGE DONE

--- tools/console.rb
+++ tools/console.rb
@@ -10,8 +10,24 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+cory = Viewer.new("coryrosser")
+harry = Viewer.new("Harry")
+dan = Viewer.new("Dan")
 
+harrypotter = Movie.new("Harry Potter")
+fastandfurious = Movie.new("Fast and Furious")
+hungergames = Movie.new("Hunger Games")
+frozen = Movie.new("Frozen")
 
+fivestarcory = Review.new(cory, harrypotter, 5)
+threestardan = Review.new(dan, hungergames, 3)
+twostardan = Review.new(dan, harrypotter, 2)
+
+fourstarharry = Review.new(harry, harrypotter, 4)
+onestarharry = Review.new(harry, hungergames, 1)
+threestarcory = Review.new(cory, fastandfurious, 3)
+
+fourstardan = Review.new(dan, frozen, 4)
 
 
 # ===== WARNING! DO NOT EDIT BELOW THIS LINE ===== #
